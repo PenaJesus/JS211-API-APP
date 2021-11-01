36283438169ab8d57431682e03e0b326b000d832
Jesus898 <jesus81398@gmail.com>
2021-10-31 20:24:06 -0500

    added brewery

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
-let arrayOfCities;
+let url = 'https://api.openbrewerydb.org/breweries?by_city=austin';
 
-window.onload = function() {
-  getBreweries();
-  byCity();
+fetchData = () => {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
+    })
+    //logging name
 
-}
-const getBreweries = () => {
-  fetch('https://api.openbrewerydb.org/breweries')
-  .then(response => response.json())
-  .then(data => console.log(data))
+
+    .then(data => {
+
+      const listOfNames = data
+      console.log(listOfNames)
+
+      listOfNames.map(brewery => {
+        console.log(brewery)
+        // document.querySelector('#name').append(brewery.name)
+
+        // Create a temporary <div> to load into
+        var div = document.createElement('div');
+        div.setAttribute('class', 'someClass');
+        div.innerHTML = document.getElementById('blockOfStuff').innerHTML;
+
+        // You could optionally even do a little bit of string templating
+        div.innerHTML = div.innerHTML
+          .replace(/name/g, brewery.name)
+          .replace(/city/g, brewery.city)
+          .replace(/street/g, brewery.street)
+          .replace(/phone/g, brewery.phone)
+          .replace(/state/g, brewery.state)
+          .replace(/btncheck/g, brewery.id);
+        // Write the <div> to the HTML container
+        document.getElementById('targetElement').appendChild(div);
+      })
+    })
+
+
+
+
+    //catching error
+    .catch(error => {
+      console.log(error);
+    })
 }
 
-const byCity = () => {
-  fetch('https://api.openbrewerydb.org/breweries?by_city=austin')
-  .then(res => res.json())
-  .then(city => console.log(city))
-}
+fetchData();
+
+
+
